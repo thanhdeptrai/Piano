a04e34362e0d4ae9c75836735aff7e6d1c773259
dotabox <box.dota@gmail.com>
2013-11-23 10:14:47 +0700

    ak

--- js/sound.js
+++ js/sound.js
@@ -2,30 +2,43 @@ Sound = {
 	initialize: function(audioDescriptor){
 		this.audioDescriptor = audioDescriptor;
 		this.audioObjects =  {};
-		for(var i=0;i<audioDescriptor.length;i++){
-			this.audioObjects[audioDescriptor[i].id] = audioDescriptor[i].audio;
-		}
 		this.sfxChannelId = [];
 		this.musicChannelId;
-		this.sfxLength = 16;
+		this.sfxLength = 25;
 	},
 	playSfx: function(soundId){
 		var self = this;
-		this.sfxChannelId.push(soundId);
-		var audio = this.audioObjects[soundId];
-		audio._onend = [function(){
-			var index = self.sfxChannelId.indexOf(audio);
-			self.sfxChannelId.splice(index,1);
-		}];
-		audio.volume(SFX_VOLUME/100);
-		audio.play();
-		this.setSfxVolume(SFX_VOLUME);
-		//console.log(this.sfxChannelId.length);
-		if(this.sfxChannelId.length>this.sfxLength){
-			var audio = this.audioObjects[this.sfxChannelId[0]];
-			audio.stop();
-			self.sfxChannelId.splice(0,1);
+		
+		if (soundId<25) {
+			var mmm=soundId%5;
+			switch(mmm){
+				case 0:soundId=(soundId/5>>0)*12+1; break;
+				case 1:soundId=(soundId/5>>0)*12+3;break;
+				case 2:soundId=(soundId/5>>0)*12+6;break;
+				case 3:soundId=(soundId/5>>0)*12+8;break;
+				case 4:soundId=(soundId/5>>0)*12+10;break;
+			}			
+		} else {
+
+			soundId-=25;
+			var mmm=soundId%7;
+			switch(mmm){
+				case 0:soundId=(soundId/7>>0)*12+0; break;
+				case 1:soundId=(soundId/7>>0)*12+2;break;
+				case 2:soundId=(soundId/7>>0)*12+4;break;
+				case 3:soundId=(soundId/7>>0)*12+5;break;
+				case 4:soundId=(soundId/7>>0)*12+7;break;
+				case 5:soundId=(soundId/7>>0)*12+9;break;
+				case 6:soundId=(soundId/7>>0)*12+11;break;
+			}
 		}
+		soundId+=36;
+		var delay = 0; // play one note every quarter second
+                       
+                        var velocity = 127; // how hard the note hits
+
+		MIDI.noteOn(0,soundId, velocity, delay);
+		MIDI.noteOff(0, soundId, delay + 0.75);
 	},
 	playMusic:function(soundId,endCallback){
 		if(this.musicChannelId) this.audioObjects[this.musicChannelId].stop();
