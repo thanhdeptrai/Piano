4c91cc7a1206e7efc05c4ebc8b7666806234282b
dotabox <box.dota@gmail.com>
2013-11-23 14:32:54 +0700

    zdfd

--- js/main.js
+++ js/main.js
@@ -13,6 +13,16 @@ musicData = [
 	
 ];
 window.onload = function () {
+		var loader = new widgets.Loader({
+		id: "loader",
+		bars: 12,
+		radius: 20,
+		lineWidth: 3,
+		lineHeight: 10,
+		message:"New loading message..."
+	});
+	
+
     var loadedImage = 0;
 	var loadedAudio = 0;
 	var loadedPercent = 0;
@@ -78,6 +88,7 @@ window.onload = function () {
 			ctx.fill();
 			if(processed&&loadImages&&(+new Date() - startTime>1000)) {
 				run(director,loadImages,loadAudios);
+				loader.stop();	
 				scene.removeChild(this);
 			}
 		}
