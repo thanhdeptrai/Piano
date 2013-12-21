a824c55d838fd9f18dc2516e1266659ab372d423
thanhdeptrai <thanhpt.sami@gmai.com>
2013-12-21 17:47:32 +0700

    up tạm

--- js/pianoKey.js
+++ js/pianoKey.js
@@ -1,25 +1,122 @@
 ﻿
 (function () {
-    CAAT.SettingContainer = function () {
-        CAAT.SettingContainer.superclass.constructor.call(this);
+    CAAT.HighScoreContainer = function () {
+        CAAT.HighScoreContainer.superclass.constructor.call(this);
         return this;
     }
 
-    CAAT.SettingContainer.prototype = {
-        initialize: function (director,posX,posY,width,height) {
-            this.director = director;
+    CAAT.HighScoreContainer.prototype = {
+        initialize: function (menuContainer,posX,posY,width,height) {
+			var self = this;
+            this.director = menuContainer.director;
+			this.menuContainer = menuContainer;
 			this.setBounds(posX,posY,width,height);
+			this.difficultyText = ["EASY","HARD","INSANE"];
+			this.playButtonWidth = 100;
+			this.playButtonHeight = 40;
+			this.playButtonY = this.height - 110;
             return this;
         },
         paint: function (director,time) {
-			CAAT.SettingContainer.superclass.paint.call(this, director, time);
+			CAAT.HighScoreContainer.superclass.paint.call(this, director, time);
             var ctx = director.ctx;
+			ctx.fillStyle = "#433";
+			ctx.fillRect(this.width-5,0,5,this.height);
+			ctx.fillStyle = "#FFF";
+			ctx.font = "25px Verdana";
+			var highScoreText = "HIGHSCORE";
+			ctx.fillText(highScoreText,this.width/2 - ctx.measureText(highScoreText).width/2,60);
+			var score = pointData[SELECTING_RECORD][DIFFICULTY]+"";
+			ctx.fillText(score,this.width/2 - ctx.measureText(score).width/2,100);
 			
+			var musicAllData = musicList[SELECTING_RECORD].Data;
+			var singleMusicData;
+			switch(DIFFICULTY){
+				case DIFFICULTY_EASY: singleMusicData = musicAllData.Easy; break;
+				case DIFFICULTY_HARD: singleMusicData = musicAllData.Hard; break;
+				case DIFFICULTY_INSANE: singleMusicData = musicAllData.Insane; break;
+			}
+			var lastIndex = singleMusicData.NodeData.lastIndexOf(" ");
+			var duration = ((singleMusicData.NodeData.substr(lastIndex+1)<<0)/1000)<<0;
+			var minute = ""+((duration/60)>>0);
+			minute = (minute.length==2)? minute : "0"+minute;
+			var second = ""+((duration%60)>>0);
+			second = (second.length==2)? second : "0"+second;
+			var timeText = minute +" : "+ second;
+			ctx.fillText(timeText,this.width/2-ctx.measureText(timeText).width/2,this.height-130);
+			
+			ctx.strokeStyle = "#FFF";
+			ctx.strokeRect(this.width/2-this.playButtonWidth/2,this.playButtonY,this.playButtonWidth,this.playButtonHeight);
+			var playText = "PLAY";
+			ctx.fillText(playText,this.width/2-ctx.measureText(playText).width/2,this.playButtonY+30);
+			for(var i=0;i<3;i++){
+				ctx.fillStyle = (i==DIFFICULTY)?"#FFF":"#BBB";
+				var text = this.difficultyText[i];
+				ctx.fillText(text,this.width/2 - ctx.measureText(text).width/2,this.height/2+(i-1)*70);
+			}
+			
+			if(this.loading){
+				ctx.globalAlpha = 0.7;
+				ctx.fillStyle = "#000";
+				ctx.fillRect(-this.x,-this.y,director.width,director.height);
+			}
             return this;
         },
-		
+		closeBehavior: function(type){
+			var self = this;
+			var path= new CAAT.PathUtil.LinearPath().
+				setInitialPosition(this.x,this.y).
+				setFinalPosition(-this.width,0);
+			var pathBehavior= new CAAT.PathBehavior().setPath(path).setFrameTime(self.time,369).
+			addListener({
+				behaviorExpired: function(director,time){
+					self.emptyBehaviorList();
+				}
+			});
+			self.addBehavior(pathBehavior);
+			if(type==2){
+				this.menuContainer.playButton.fn();
+			}
+		},
+		mouseDown: function(e){
+			var self = this;
+			if((e.x>this.width/4)&&(e.x<this.width*3/4)){
+				for(var i=0;i<3;i++){
+					if((e.y>this.height/2+(i-1)*70-50)&&(e.y<this.height/2+i*70-50)){
+						DIFFICULTY = i;
+						break;
+					}
+				}
+			}
+			if((e.x>this.width/2-this.playButtonWidth/2)&&(e.x<this.width/2+this.playButtonWidth/2)
+				&&(e.y>this.playButtonY)&&(e.y<this.playButtonY+this.playButtonHeight)){
+				self.menuContainer.stopButton.fn();
+				self.loading = true;
+				self.menuContainer.playListContainer.loading = true;
+				self.enableEvents(false);
+				self.menuContainer.playListContainer.enableEvents(false);
+				var musicAllData = musicList[SELECTING_RECORD].Data;
+				var singleMusicData;
+				switch(DIFFICULTY){
+					case DIFFICULTY_EASY: singleMusicData = musicAllData.Easy; break;
+					case DIFFICULTY_HARD: singleMusicData = musicAllData.Hard; break;
+					case DIFFICULTY_INSANE: singleMusicData = musicAllData.Insane; break;
+				}
+				var audioLink;
+				if((!PLAY_FULL_FILE)&&(singleMusicData.Simple))audioLink = singleMusicData.Simple;
+				else audioLink = singleMusicData.Full;
+				Sound.playMusic(audioLink,
+				function(){
+					self.loading = false;
+					self.menuContainer.playListContainer.loading = false;
+					playingAudio = Sound.audioMusic;
+					self.menuContainer.closeBehavior(2);
+				},true);
+				
+			}
+		}
     }
-    extend(CAAT.SettingContainer, CAAT.Foundation.ActorContainer);
+    extend(CAAT.HighScoreContainer, CAAT.Foundation.ActorContainer);
 })();
 
 (function () {
@@ -30,21 +127,73 @@
 
     CAAT.PlayListContainer.prototype = {
         initialize: function (menuContainer,posX,posY,width,height) {
+			var self = this;
             this.director = menuContainer.director;
 			this.menuContainer = menuContainer;
 			this.setBounds(posX,posY,width,height);
+			this.textStartX = 70;
+			this.textStartY = 180;
+			this.textHeight = 60;
+			this.offButtonPosition = {x:40,y:40};
+			this.circleRadius = this.textHeight*0.3;
             return this;
         },
         paint: function (director,time) {
 			CAAT.PlayListContainer.superclass.paint.call(this, director, time);
             var ctx = director.ctx;
 			
+			var font = "23px Arial";
+			ctx.font = "bold "+font;
+			ctx.fillStyle = "#FFF";
+			
+			var headX = this.offButtonPosition.x;
+			var headY = this.offButtonPosition.y;
+			var textPosY = headY+10;
+			var text = "SONG LIST";
+			ctx.fillText(text,this.width/2 - ctx.measureText(text).width/2,textPosY);
+			var textStartX = this.textStartX;
+			var textStartY = this.textStartY;
+			var textHeight = this.textHeight;
+			
+			ctx.strokeStyle = "#FFF";
+			ctx.fillStyle = "#ECC";
+			
+			this.drawCircle(ctx,headX,headY,this.circleRadius);
+			ctx.fillText("X",headX-ctx.measureText("X").width/2,textPosY);
+			ctx.fillRect(0,textStartY+textHeight*SELECTING_RECORD - textHeight*2/3,this.width,textHeight);
+			for(var i=0;i<musicList.length;i++){
+				ctx.fillStyle = (i==SELECTING_RECORD)?"#000":"#FFF";
+				ctx.strokeStyle = (i==SELECTING_RECORD)?"#000":"#FFF";
+				ctx.font = font;
+				this.drawCircle(ctx,headX,textStartY+i*textHeight - textHeight/6,this.circleRadius);
+				var textPosY = textStartY+i*textHeight;
+				ctx.fillText(musicList[i].Name.toUpperCase(),textStartX,textPosY);
+				ctx.font = "bold "+font;
+				var numberText = ""+(i+1);
+				ctx.fillText(numberText,headX-ctx.measureText(numberText).width/2,textPosY);
+			}
+			if(this.loading){
+				var loadText = "LOADING.";
+				var dotNumber = ((time/500)<<0)%4;
+				for(var i=0;i<dotNumber;i++) loadText+="."
+				ctx.fillText(loadText,textStartX,this.height-50);
+			}
             return this;
         },
-		mouseDown: function(e){
-			CAAT.PlayListContainer.superclass.mouseDown.call(this,e);
+		drawCircle: function(ctx,centerX,centerY,radius){
+			ctx.save();
+			ctx.beginPath()
+			ctx.arc(centerX,centerY,radius,0,2*Math.PI);
+			ctx.closePath();
+			ctx.stroke();
+			ctx.restore();
+		},
+		checkMouseInCircle: function(ex,ey,centerX,centerY,radius){
+			if(Math.pow(centerX-ex,2)+Math.pow(centerY-ey,2)<Math.pow(radius,2)) return true;
+			return false;
+		},
+		closeBehavior: function(type){
 			var self = this;
-			console.log(e.x+" "+e.y);
 			var path= new CAAT.PathUtil.LinearPath().
 				setInitialPosition(this.x,this.y).
 				setFinalPosition(this.director.width,0);
@@ -55,6 +204,23 @@
 				}
 			});
 			self.addBehavior(pathBehavior);
+			if(type == 1){
+				SELECTING_RECORD = this.currentRecord;
+			}
+		},
+		mouseDown: function(e){
+			CAAT.PlayListContainer.superclass.mouseDown.call(this,e);
+			var self = this;
+			if(this.checkMouseInCircle(e.x,e.y,this.offButtonPosition.x,this.offButtonPosition.y,this.circleRadius)){
+				this.menuContainer.closeBehavior(1);
+			}
+			var startY = this.textStartY - this.textHeight*2/3;
+			for(var i=0;i<musicList.length;i++){
+				if((e.y>startY+i*this.textHeight)&&(e.y<startY+(i+1)*this.textHeight)){
+					SELECTING_RECORD = i;
+					break;
+				}
+			}
 		}
 		
     }
@@ -83,36 +249,59 @@
 			this.marginLeft = 5;
 			this.lineHeight = 20;
 			this.inAnimation = false;
-			var playListImage =  new CAAT.SpriteImage().initialize(director.getImage("recordButton"),1,3);
+			var playListImage =  new CAAT.SpriteImage().initialize(director.getImage("playListButton"),1,3);
 			var playListButton = new CAAT.Button().initialize(director,playListImage,0,1,2,0,
 			function(e){
 				if(self.inAnimation) return;
-				
+				if(PLAYING_RECORD&&(!PAUSING_RECORD)){
+					self.playButton.fn();
+				}
 				self.inAnimation = true;
-				if(!self.firstTime)self.playListContainer = new CAAT.PlayListContainer().initialize(self,director.width/2,self.y,director.width/2,self.height).setFillStyle(self.fillStyle);
-				else self.playListContainer.setLocation(director.width/2,self.y);
+				playListPosX = director.width/2 - 80;
+				playListWidth = director.width - playListPosX;
+				if(!self.firstTime){
+					self.playListContainer = new CAAT.PlayListContainer().initialize(self,playListPosX,self.y,playListWidth,self.height).setFillStyle(self.fillStyle);
+					self.highScoreContainer = new CAAT.HighScoreContainer().initialize(self,0,self.y,playListPosX,self.height).setFillStyle("#444");
+				}
+				else {
+					self.playListContainer.setLocation(playListPosX,self.y);
+					self.highScoreContainer.setLocation(0,self.y);
+					self.highScoreContainer.enableEvents(true);
+					self.playListContainer.enableEvents(true);
+				}
+				self.playListContainer.currentRecord = SELECTING_RECORD;
 				var path= new CAAT.PathUtil.LinearPath().
-					setInitialPosition(director.width,0).
+					setInitialPosition(director.width,self.y).
 					setFinalPosition(self.playListContainer.x,self.playListContainer.y);
 				var pathBehavior= new CAAT.PathBehavior().setPath( path ).setFrameTime(self.time,369).
 				addListener({
 					behaviorExpired: function(director,time){
 						self.inAnimation = false;
-						//self.playListContainer.setBounds(self.playListContainer.x,self.playListContainer.y,self.playListContainer.width,self.playListContainer.height);
-						//self.playListContainer.emptyBehaviorList();
 					}
 				});
 				self.playListContainer.addBehavior(pathBehavior);
+				
+				var path2= new CAAT.PathUtil.LinearPath().
+					setInitialPosition(-self.highScoreContainer.width,self.y).
+					setFinalPosition(self.highScoreContainer.x,self.highScoreContainer.y);
+				var pathBehavior2= new CAAT.PathBehavior().setPath( path2 ).setFrameTime(self.time,369);
+				self.highScoreContainer.addBehavior(pathBehavior2);
+				
 				if(!self.firstTime){
 					self.director.currentScene.addChild(self.playListContainer);
+					self.director.currentScene.addChild(self.highScoreContainer);
 					self.firstTime = true;
 				}
-			}).setLocation(this.width/2 - playListImage.singleWidth);
+			}).setLocation(this.width/2 - playListImage.singleWidth/2,20);
 			
 			this.playListButton = playListButton;
 			this.addChild(playListButton);
             return this;
         },
+		closeBehavior:function(type){
+			this.playListContainer.closeBehavior(type);
+			this.highScoreContainer.closeBehavior(type);
+		},
         paint: function (director,time) {
 			CAAT.MenuContainer.superclass.paint.call(this, director, time);
             var ctx = director.ctx;
--- js/sound.js
+++ js/sound.js
@@ -4,17 +4,18 @@
         return this;
     }
  CAAT.SoundPiano.prototype  = {
+	
 	initialize: function(audioDescriptor){
 		this.audioDescriptor = audioDescriptor;
 		this.audioObjects =  {};
 		this.sfxChannelId = [];
 		this.musicChannelId;
+		this.audioMusic;
 		this.sfxLength = 25;
 		return this;
 	},
 	playSfx: function(soundId){
 		var self = this;
-		
 		if (soundId<25) {
 			var mmm=soundId%5;
 			switch(mmm){
@@ -47,24 +48,52 @@
 		MIDI.noteOff(0, soundId, delay + 5);
 		return this;
 	},
-	playMusic:function(soundId,endCallback){
-		if(this.musicChannelId) this.audioObjects[this.musicChannelId].stop();
-		this.musicChannelId = soundId;
+	playMusic: function(soundId,canplayCallback,justLoad){
+		var self = this;
+		this.musicChannelId = (soundId);
 		var audio = this.audioObjects[soundId];
-		if(endCallback) {			
-			audio.loop(false);
-			audio._onend = [endCallback];
+		var audioDescriptor = this.audioDescriptor;
+		if(typeof audio == "undefined"){
+			var i = 0;
+			for(i;i<audioDescriptor.length;i++){
+				if(audioDescriptor[i].id==soundId){
+					break;
+				}
+			}
+			audio = new Audio(audioDescriptor[i].path);
+			this.audioObjects[audioDescriptor[i].id] = audio;
+			if(!audio){
+				return;
+			}
+			audio.addEventListener("canplay", function(){
+				console.log("audio start");
+				if(canplayCallback) canplayCallback();
+				if(!justLoad){
+					audio.volume=(SFX_VOLUME/100);
+					audio.play();
+				}
+			});
 		}
-		else audio.loop(true);
-		audio.volume(MUSIC_VOLUME/100);
-		audio.play();
-		this.setMusicVolume(MUSIC_VOLUME);
-		return this;
+		else{
+			if(canplayCallback) canplayCallback();
+			if(!justLoad){
+				audio.volume=(SFX_VOLUME/100);
+				audio.play();
+			}
+		}
+		audio.addEventListener("ended", function(){
+			console.log("audio ended");
+			self.musicChannelId = undefined;
+		});
 		
+		this.audioMusic = audio;
 	},
-	endSound: function(){
-		if(this.musicChannelId) this.audioObjects[this.musicChannelId].stop();
-		return this;
+	endMusic: function(){
+		if(this.musicChannelId){
+			if(this.musicChannelId) this.musicChannelId.pause();
+			if(this.musicChannelId.currentTime) this.musicChannelId.currentTime = 0;
+		}
+		this.musicChannelId = undefined;
 	},
 	setSfxVolume: function(value){
 		SFX_VOLUME = value;
@@ -85,3 +114,94 @@
 }
  extend(CAAT.SoundPiano, CAAT.Foundation.ActorContainer);
 })();
+function get_browser(){
+    var N=navigator.appName, ua=navigator.userAgent, tem;
+    var M=ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
+    if(M && (tem= ua.match(/version\/([\.\d]+)/i))!= null) M[2]= tem[1];
+    M=M? [M[1], M[2]]: [N, navigator.appVersion, '-?'];
+    return M[0];
+}
+function getExtension(src){
+	return src.substr(src.lastIndexOf(".")+1);
+}
+function getName(src){
+	return src.substr(src.lastIndexOf("/")+1);
+}
+var browserCheck = [];
+browserCheck["ogg"] = ["Firefox","Opera","Chrome"];
+browserCheck["mp3"] = ["Chrome","Safari"];
+checkBrowser = function(extension){
+	if(browserCheck[extension].indexOf(get_browser()!=-1)){
+		return true;
+	}
+	else return false;
+}
+
+AudioPreloader = function() {
+	var descriptor= function(id, path, loader) {
+		var self = this;
+		this.id=    id;
+		
+		this.loader= loader;
+		this.load = function(){
+			if(typeof path == "string") {
+				self.path = path;
+			}
+			else{
+				for(var i=0;i<path.length;i++){
+					if(checkBrowser(getExtension(path[i]))){
+						self.path = path[i];
+						break;
+					}
+				}
+				if(!self.path){
+					for(var i=0;i<path.length;i++){
+						if(getExtension(path[i]=="mp3")){
+							self.path = path[i];
+							break;
+						}
+					}
+				}
+				if(!self.path) self.path = path[0];
+			}
+			//console.log(self.path);
+			self.loader.onloadOne(self)
+		}
+		return this;
+	};
+	return {
+		elements: [],
+		loadedCount:0,
+		addElement : function( id, path ) {
+			if(!path) path = id;
+			this.elements.push( new descriptor(id,path,this));
+			return this;
+		},
+
+		onloadOne : function( descriptor ) {
+			if ( this.cloaded ) {
+				this.cloaded(descriptor.id);
+			}
+			this.loadedCount++;
+			
+			if ( this.loadedCount===this.elements.length ) {
+				if ( this.cfinished ) {
+					this.cfinished( this.elements );
+				}
+			}
+		},
+
+		load: function( onfinished, onload_one) {
+
+			this.cfinished= onfinished;
+			this.cloaded= onload_one;
+			
+			for(var i=0; i<this.elements.length; i++ ) {
+				this.elements[i].load();
+			}
+
+			return this;
+		}
+	}
+}
+
